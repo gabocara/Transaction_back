4c547b458c4dbffa736296d1d57babe7a7ea0275
Gabriel Caradonna <gabo.cara@gmail.com>
2022-05-14 14:56:28 -0300

    add mapper, change transactions services

--- src/main/java/com/mendel/challenge/ChallengeApplication.java
+++ src/main/java/com/mendel/challenge/ChallengeApplication.java
@@ -1,9 +1,11 @@
 package com.mendel.challenge;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan("com.mendel.challenge")
@@ -14,4 +16,9 @@ public class ChallengeApplication {
 		SpringApplication.run(ChallengeApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper;
+	}
 }
--- src/main/java/com/mendel/challenge/services/impl/TransactionService.java
+++ src/main/java/com/mendel/challenge/services/impl/TransactionService.java
@@ -1,13 +1,18 @@
 package com.mendel.challenge.services.impl;
 
+import com.mendel.challenge.models.Transaction;
 import com.mendel.challenge.repository.TransactionRepository;
 import com.mendel.challenge.requests.TransactionRequest;
 import com.mendel.challenge.responses.TransactionResponse;
 import com.mendel.challenge.services.ITransactionService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -15,13 +20,24 @@ public class TransactionService implements ITransactionService {
 
     private final TransactionRepository transactionRepository;
 
+    private final ModelMapper modelMapper;
+
     @Override
     public TransactionResponse getTransactionsByType(String transactionType) {
-        return transactionRepository.getTransactionsByType(transactionType);
+        List<Transaction> transactions = transactionRepository.getTransctions();
+        return new TransactionResponse(transactions.stream().filter(transaction ->  transaction.getTransactionType().equalsIgnoreCase(transactionType)));
+
     }
 
     @Override
     public List<TransactionResponse> createTransaction(TransactionRequest transactionRequest) {
-         return transactionRepository.createTransaction(transactionRequest);
+        List<Transaction> transactionList = transactionRepository.createTransaction(transactionRequest);
+        return transactionList.stream().map(this::convertToTransactionResponse).collect(Collectors.toUnmodifiableList());
+    }
+
+    private TransactionResponse convertToTransactionResponse(Transaction transaction){
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        TransactionResponse transactionResponse = modelMapper.map(transaction, TransactionResponse.class);
+        return transactionResponse;
     }
 }
